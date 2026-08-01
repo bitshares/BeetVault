82bba71a4b9698d5a84edb95675b4beddca0423c
CM <grctestemail@gmail.com>
2026-08-01 14:04:01 +0100

    Update BeetDB.js
    Avoid dexie collision with existing beet wallets

--- src/lib/BeetDB.js
+++ src/lib/BeetDB.js
@@ -1,5 +1,5 @@
 import Dexie from 'dexie';
-const BeetDB = new Dexie('BeetDB');
+const BeetDB = new Dexie('BeetVaultDB'); // Change name if you fork the wallet to avoid wiping data from the original wallet
 
 BeetDB.version(1).stores({
     settings: `++id, setting, value`,
